2a261c364004764344515d8a288f4ab2467c05e2
Juh Simoes <42075263+JuhJSimoes@users.noreply.github.com>
2022-02-08 08:25:13 -0300

    Main
    Ajuste da main

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from cpf_cnpj import CpfCnpj
+from cpf_cnpj import Documento
 
 #cpf_um = Cpf("05801021736")
 #print(cpf_um)
 
-exemplo_cnpj = "78190140000167"
-documento = CpfCnpj(exemplo_cnpj, 'cnpj')
+#exemplo_cnpj = "78190140000167"
+#documento = Documento.cria_documento(exemplo_cnpj)
+
+exemplo_cpf = "05801021736"
+documento = Documento.cria_documento(exemplo_cpf)
+
 
 print(documento)
 
