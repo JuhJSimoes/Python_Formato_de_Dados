afa46946a6de9e70286199701af828cff48b2e40
JuhJSimoes <julianajsimoes@gmail.com>
2022-02-09 07:30:08 -0300

    ValidacaoTelefone

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from cpf_cnpj import Documento
+#from cpf_cnpj import Documento
+from telefoneBr import TelefonesBr
+
+
+
+#Bloco de teste CNPJ e CPF
 
 #exemplo_cnpj = "78190140000167"
 #documento = Documento.cria_documento(exemplo_cnpj)
+#exemplo_cpf = "05801021736"
+#documento = Documento.cria_documento(exemplo_cpf)
+#print(documento)
 
-exemplo_cpf = "05801021736"
-documento = Documento.cria_documento(exemplo_cpf)
+#Bloco de teste telefones / necessario ajusto quando for celular
+telefone = '552133091008'
 
+telefone_obj = TelefonesBr(telefone)
+print(telefone_obj)
 
-print(documento)
 
---
+++ telefoneBr.py
@@ -0,0 +1,33 @@
+import re
+
+class TelefonesBr:
+    
+    def __init__(self, telefone):
+        if self.valida_telefone(telefone):
+            self.numero = telefone
+        else:
+            raise ValueError("Numero incorreto")
+    
+    def __str__(self):
+        return self.format_telefone()
+    
+    def valida_telefone(self, telefone):
+        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4})([0-9]{4})"
+        #texto = "testando vazio 2133091008"
+        resposta = re.findall(padrao, telefone)
+        if resposta:
+            return True
+        else:
+            return False
+    
+    def format_telefone(self):
+        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        resposta = re.search(padrao, self.numero)
+        numero_formatado = "+{} ({}) {}-{}".format(
+            resposta.group(1), 
+            resposta.group(2), 
+            resposta.group(3), 
+            resposta.group(4)
+        )
+        
+        return numero_formatado
